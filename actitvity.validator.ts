e61ea27cee30b08f6900e3873978424096193285
IssaDia <44264590+IssaDia@users.noreply.github.com>
2024-07-28 13:08:58 +0200

    add new output for activity

--- actitvity.validator.ts
+++ actitvity.validator.ts
@@ -1,38 +1,61 @@
 import { z } from 'zod';
 import { zodToJsonSchema } from 'zod-to-json-schema';
+import {
+  paginationResponseSchema,
+  paginationSchema,
+  response500Schema,
+} from '.';
 
-const activity_activity_type = z.enum(['outdoor', 'indoor']); // Example enum, adjust as needed
+const activity_activity_type = z.enum(['outdoor', 'indoor']);
 
 const positiveNumberRegex = /^[1-9]\d*$/;
 
-export const FetchActivitiesInputSchema = z.object({
-  query: z.object({
-    filters: z
-      .object({
-        startDate: z.date().optional(),
-        location: z.string().optional(),
-        activityType: activity_activity_type.optional(),
-        search: z.string().optional(),
-      })
-      .optional(),
-    pagination: z
-      .object({
-        pageSize: z
-          .string()
-          .regex(positiveNumberRegex, 'pageSize must be a positive number'),
-        page: z
-          .string()
-          .regex(positiveNumberRegex, 'page must be a positive number'),
-      })
-      .optional(),
-    sort: z
-      .object({
-        id: z.enum(['asc', 'desc']),
-      })
-      .optional(),
-  }),
+export const fetchActivitiesInputSchema = z.object({
+  query: z
+    .object({
+      filters: z
+        .object({
+          startDate: z.date().optional(),
+          location: z.string().optional(),
+          activityType: activity_activity_type.optional(),
+          search: z.string().optional(),
+        })
+        .optional(),
+
+      sort: z
+        .object({
+          id: z.enum(['asc', 'desc']),
+        })
+        .optional(),
+    })
+    .merge(paginationSchema),
 });
 
+export const fetchActivitiesResponseSchema = z
+  .object({
+    response200: z.object({
+      success: z.boolean(),
+      data: z
+        .object({
+          activities: z
+            .object({
+              id: z.number(),
+              name: z.string(),
+              description: z.string(),
+              location: z.string(),
+              startDate: z.string(),
+              endDate: z.string(),
+              activityType: z.string(),
+              image: z.string(),
+            })
+            .array(),
+        })
+        .merge(paginationResponseSchema),
+      message: z.string(),
+    }),
+  })
+  .merge(response500Schema);
+
 export type FetchActivitiesInputSchemaType = z.infer<
-  typeof FetchActivitiesInputSchema
+  typeof fetchActivitiesInputSchema
 >;
--- index.ts
+++ index.ts
@@ -6,3 +6,20 @@ export const response500Schema = z.object({
     message: z.string(),
   }),
 });
+
+export const paginationSchema = z.object({
+  pagination: z
+    .object({
+      pageSize: z
+        .string()
+        .regex(/^[1-9]\d*$/, 'pageSize must be a positive number'),
+      page: z.string().regex(/^[1-9]\d*$/, 'page must be a positive number'),
+    })
+    .optional(),
+});
+
+export const paginationResponseSchema = z.object({
+  pageSize: z.number(),
+  page: z.number(),
+  total: z.number(),
+});
